efce7c6db50be18687bca8f8edee3a45ade27358
Maksim Dzjubenko <maksondzjubenko@gmail.com>
2024-05-11 00:35:03 +0300

    Added env files

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,4 @@
-// export const API_SERVER = 'http://localhost:8080/api/v1';
-// export const WEBSOCKET_URL = 'ws://localhost:8080/api/v1/ws';
 export const API_SERVER = 'https://skillshuffle.ddns.net/api/v1';
 export const WEBSOCKET_URL = 'wss://skillshuffle.ddns.net/api/v1/ws';
-export const ENCRYPTION_KEY = 'svin-genius-encryption-iv';
-export const TENOR_API_KEY = 'AIzaSyB1FyUaE2VWYs5Koq_aAded3FovuxdzIlk';
+export const ENCRYPTION_KEY = process.env.REACT_APP_ENCRYPTION_KEY;
+export const TENOR_API_KEY = process.env.REACT_APP_TENOR_API_KEY;
